6ecdc7082fc411bd158f488bf6ba905861da43b4
unknown <vhimabindu22@gmail.com>
2021-11-04 15:44:16 +0530

    added new

---
+++ Youtube/Courses/Learn React/Hands On/hello/helloworld/src/Body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Body extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+          <h1>Hello Body</h1>
+            </>
+        )
+    }
+}
+
+
+export default Body;
